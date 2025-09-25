c10eb0645cc9a408a2ad78358afb2473b7a3d6eb
linzhanguca <lzhang12@uca.edu>
2025-09-25 10:02:38 -0500

    update readme with wiring guides

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 3421example-ultrasonic_sensor
-HC-SR04 usage examples 
+[HC-SR04](https://cdn.sparkfun.com/datasheets/Sensors/Proximity/HCSR04.pdf) usage examples 
+
+## 1 Wiring
+### 1.1 Powered by Pico's Micro-USB Connection
+![wiring-usb](images/hcsr04_wiring-usb.png)
+
+### 1.2 Powered by External 5V Source (e.g. Buck Converter)
+![wiring-peb](images/hcsr04_wiring-peb.png)
--- baisc_ranging.py
+++ baisc_ranging.py
@@ -1,9 +1,9 @@
-from machine import Pin, PWM, Timer, reset
+from machine import Pin
 from time import sleep_us, sleep_ms, ticks_us
 
 # SETUP
-trig = Pin(7, Pin.OUT)
-echo = Pin(6, Pin.IN, Pin.PULL_DOWN)
+trig = Pin(3, Pin.OUT)
+echo = Pin(2, Pin.IN, Pin.PULL_DOWN)
 # Config IRQ
 tic, toc, distance = None, None, None
 def resolve_status(pin):
@@ -14,10 +14,10 @@ def resolve_status(pin):
         dt = ticks_us() - tic
         if dt < 100:
             distance = 0.
-        elif 100 <= dt < 36000:
+        elif 100 <= dt < 38000:
             distance = dt / 58 / 100
         else:
-            distance = 100.
+            distance = None
 echo.irq(handler=resolve_status)
 
 # LOOP
@@ -26,5 +26,5 @@ while True:
     sleep_us(10)
     trig.off()  # 10 us pulse to trigger
     print(f"distance: {distance} m")
-    sleep_ms(100)
+    sleep_ms(60)
 
