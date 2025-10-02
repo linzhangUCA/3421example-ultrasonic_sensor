fe9b8e02fc89d01cb0e67c802f9ea95054f56ab7
linzhanguca <lzhang12@uca.edu>
2025-10-02 11:50:15 -0500

    add an example test script

---
+++ test_distance_sensor.py
@@ -0,0 +1,14 @@
+from distance_sensor import DistanceSensor
+from time import sleep_ms
+from machine import reset
+
+# SETUP
+sensor = DistanceSensor(trig_id=18, echo_id=19)
+
+# LOOP
+try:
+    while True:
+        print(f"Distance: {sensor.distance()} m")
+        sleep_ms(200)
+except KeyboardInterrupt:
+    reset()
